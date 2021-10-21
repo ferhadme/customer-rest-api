67c7fd8bb4f7e9177d6428d9956c482affeb71e8
ferhadme <frhdmehdiyev@gmail.com>
2021-10-21 17:44:36 +0400

    init

---
+++ src/main/java/com/ferhad/customerrestapi/CustomerRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ferhad.customerrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferhad/customerrestapi/CustomerRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferhad.customerrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
